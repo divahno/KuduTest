154154a0323e05279ca8c1ecdd58147abbf1a1b7
dmitry@pdfshareforms.com <dmitry@pdfshareforms.com>
2016-12-09 15:41:48 -0800

    initial app commit

---
+++ src/KuduTestWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KuduTestWebApp.MvcApplication" Language="C#" %>
---
+++ src/KuduTestWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KuduTestWebApp.Startup))]
+namespace KuduTestWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
